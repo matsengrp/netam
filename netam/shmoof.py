fce79651550c906dee788b47aa7694e59357fc3e
Erick Matsen <ematsen@gmail.com>
2023-11-13 09:10:38 -0800

    adding marginalization over padded kmers

--- netam/shmoof.py
+++ netam/shmoof.py
@@ -10,6 +10,8 @@ from torch.utils.data import Dataset, DataLoader
 
 import itertools
 
+BASES = ["A", "C", "G", "T"]
+
 
 def generate_kmers_with_padding(kmer_length, max_padding_length):
     """
@@ -30,37 +32,22 @@ def generate_kmers_with_padding(kmer_length, max_padding_length):
     list of str
         A sorted list of unique k-mers. Each k-mer is a string of length `kmer_length`,
         composed of 'A', 'C', 'G', 'T', and up to `max_padding_length` 'N's at either
-        the beginning or end of the k-mer. The list is sorted alphabetically.
-
-    Examples
-    --------
-    >>> generate_kmers_with_padding(3, 1)
-    ['AAN', 'ACN', 'AGN', 'ANN', 'ATN', 'CAA', 'CAC', 'CAG', 'CAN', 'CAT', 'CCA', ...]
-
-    >>> generate_kmers_with_padding(3, 2)
-    ['AAN', 'ACN', 'AGN', 'ANN', 'ATN', 'CAA', 'CAC', 'CAG', 'CAN', 'CAT', 'CCA', ...]
-
-    Notes
-    -----
-    The function generates k-mers that include all combinations of 'A', 'C', 'G', 'T',
-    and also considers k-mers with up to `max_padding_length` 'N's at the start or end.
-    K-mers with 'N's in positions other than the start or end, or with more than
-    `max_padding_length` 'N's, are not included.
+        the beginning or end of the k-mer. The list is generated in lexicographic order
+        for the unpadded k-mers and then all of the padded k-mers are appended to the
+        end of the list.
     """
-    bases = ["A", "C", "G", "T"]
-    all_kmers = set()
 
     # Add all combinations of ACGT
-    all_kmers.update("".join(p) for p in itertools.product(bases, repeat=kmer_length))
+    all_kmers = ["".join(p) for p in itertools.product(BASES, repeat=kmer_length)]
 
     # Add combinations with up to max_padding_length Ns at the beginning or end
     for n in range(1, max_padding_length + 1):
-        for kmer in itertools.product(bases, repeat=kmer_length - n):
+        for kmer in itertools.product(BASES, repeat=kmer_length - n):
             kmer_str = "".join(kmer)
-            all_kmers.add("N" * n + kmer_str)
-            all_kmers.add(kmer_str + "N" * n)
+            all_kmers.append("N" * n + kmer_str)
+            all_kmers.append(kmer_str + "N" * n)
 
-    return sorted(all_kmers)
+    return all_kmers
 
 
 class SHMoofDataset(Dataset):
@@ -72,6 +59,11 @@ class SHMoofDataset(Dataset):
 
         self.all_kmers = generate_kmers_with_padding(kmer_length, self.overhang_length)
         assert len(self.all_kmers) < torch.iinfo(torch.int32).max
+        self.resolved_kmer_count = 4**kmer_length
+        for resolved_kmers in self.all_kmers[: self.resolved_kmer_count]:
+            assert "N" not in resolved_kmers
+        for padded_kmers in self.all_kmers[self.resolved_kmer_count :]:
+            assert "N" in padded_kmers
 
         self.kmer_to_index = {kmer: idx for idx, kmer in enumerate(self.all_kmers)}
 
@@ -80,6 +72,10 @@ class SHMoofDataset(Dataset):
             self.masks,
             self.mutation_vectors,
         ) = self.encode_sequences(dataframe)
+
+    @property
+    def resolved_kmers(self):
+        return self.all_kmers[: self.resolved_kmer_count]
 
     def __len__(self):
         return len(self.encoded_parents)
@@ -117,6 +113,8 @@ class SHMoofDataset(Dataset):
         # Truncate according to max_length; we'll handle the overhangs later.
         padded_sequence = padded_sequence[: self.max_length + 2 * self.overhang_length]
 
+        # Here we use a default value of 0, which wouldn't be a good idea except for
+        # the fact that we do masking to ignore these values.
         kmer_indices = [
             self.kmer_to_index.get(padded_sequence[i : i + self.kmer_length], 0)
             for i in range(self.max_length)
@@ -145,29 +143,73 @@ class SHMoofDataset(Dataset):
 
 
 class SHMoofModel(nn.Module):
-    def __init__(self, dataset):
+    def __init__(self, dataset, embedding_dim):
         super(SHMoofModel, self).__init__()
+        self.kmer_to_index = dataset.kmer_to_index
         self.kmer_count = len(dataset.kmer_to_index)
+        self.embedding_dim = embedding_dim
         self.site_count = dataset.max_length
 
-        # Initialize log rate embedding layers
-        self.log_kmer_rates = nn.Embedding(self.kmer_count, 1)
+        # Limit embedding size to the number of fully specified k-mers
+        self.kmer_embedding = nn.Embedding(
+            dataset.resolved_kmer_count, self.embedding_dim
+        )
+        self.padded_kmer_to_kmer_index_map = (
+            self.generate_padded_kmer_to_kmer_index_map()
+        )
+
         self.log_site_rates = nn.Embedding(self.site_count, 1)
 
+    def generate_padded_kmer_to_kmer_index_map(self):
+        """Precompute the indices of all k-mers that can be substituted for each k-mer."""
+        mapping = {}
+        for kmer, idx in self.kmer_to_index.items():
+            if "N" in kmer:
+                resolved_kmers = self.resolve_padded_kmer(kmer)
+                resolved_kmer_indices = [
+                    self.kmer_to_index[sub] for sub in resolved_kmers
+                ]
+            else:
+                resolved_kmer_indices = [idx]
+            mapping[idx] = resolved_kmer_indices
+        return mapping
+
+    def resolve_padded_kmer(self, kmer):
+        """Find all possible resolved kmers for a kmer with some N padding."""
+        resolved_kmers = []
+        for replacement_bases in itertools.product("ACGT", repeat=kmer.count("N")):
+            temp_kmer = kmer
+            # Replace the N's, one at a time.
+            for base in replacement_bases:
+                temp_kmer = temp_kmer.replace("N", base, 1)
+            resolved_kmers.append(temp_kmer)
+        return resolved_kmers
+
+    def get_log_kmer_rates(self, encoded_parent):
+        log_kmer_rates = torch.empty((encoded_parent.size(0), self.embedding_dim))
+
+        for i, idx in enumerate(encoded_parent):
+            kmer_indices = self.padded_kmer_to_kmer_index_map[idx.item()]
+            averaged_log_rates = self.kmer_embedding(
+                torch.tensor(kmer_indices, dtype=torch.int32)
+            ).mean(dim=0)
+            log_kmer_rates[i] = averaged_log_rates
+
+        return log_kmer_rates.squeeze()
+
     def forward(self, encoded_parent):
-        log_kmer_rates = self.log_kmer_rates(encoded_parent).squeeze()
+        log_kmer_rates = self.get_log_kmer_rates(encoded_parent).squeeze()
         positions = torch.arange(encoded_parent.size(0), device=encoded_parent.device)
         log_site_rates = self.log_site_rates(positions).squeeze()
 
         # Rates are the product of kmer and site rates.
         rates = torch.exp(log_kmer_rates + log_site_rates)
-
         return rates
 
     @property
     def kmer_rates(self):
         # Convert kmer log rates to linear space
-        return torch.exp(self.log_kmer_rates.weight).squeeze()
+        return torch.exp(self.kmer_embedding.weight).squeeze()
 
     @property
     def site_rates(self):
@@ -180,7 +222,7 @@ class SHMoofBurrito:
         self.train_dataset = SHMoofDataset(train_dataframe, kmer_length, max_length)
         self.val_dataset = SHMoofDataset(val_dataframe, kmer_length, max_length)
 
-        self.model = SHMoofModel(self.train_dataset)
+        self.model = SHMoofModel(self.train_dataset, embedding_dim=1)
         self.criterion = nn.BCELoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
 
@@ -228,13 +270,10 @@ class SHMoofBurrito:
         kmer_mutabilities = self.model.kmer_rates.detach().numpy().flatten()
         motif_mutabilities = pd.DataFrame(
             {
-                "Motif": list(self.train_dataset.kmer_to_index.keys()),
+                "Motif": self.train_dataset.resolved_kmers,
                 "Mutability": kmer_mutabilities,
             }
         )
-        motif_mutabilities = motif_mutabilities[
-            ~motif_mutabilities["Motif"].str.contains("N")
-        ]
         motif_mutabilities.to_csv(
             f"{out_dir}/motif_mutabilities.tsv", sep="\t", index=False
         )
--- tests/test_shmoof.py
+++ tests/test_shmoof.py
@@ -1,30 +1,53 @@
+import itertools
+
 import pandas as pd
 import pytest
 import torch
 
-from netam.shmoof import SHMoofDataset, SHMoofModel, SHMoofBurrito
+from netam.shmoof import SHMoofDataset, SHMoofModel, SHMoofBurrito, BASES
+
 
 @pytest.fixture
 def tiny_dataset():
     df = pd.DataFrame({"parent": ["ATGTA", "GTAC"], "child": ["ACGTA", "ATAC"]})
     return SHMoofDataset(df, max_length=6, kmer_length=3)
 
+
+@pytest.fixture
+def tiny_model(tiny_dataset):
+    return SHMoofModel(tiny_dataset, embedding_dim=1)
+
+
 def test_make_dataset(tiny_dataset):
     encoded_parent, mask, mutation_indicator = tiny_dataset[0]
     assert (mask == torch.tensor([1, 1, 1, 1, 1, 0], dtype=torch.bool)).all()
     # First kmer is NAT due to padding
     assert encoded_parent[0].item() == tiny_dataset.kmer_to_index["NAT"]
-    assert (mutation_indicator == torch.tensor([0, 1, 0, 0, 0, 0], dtype=torch.bool)).all()
+    assert (
+        mutation_indicator == torch.tensor([0, 1, 0, 0, 0, 0], dtype=torch.bool)
+    ).all()
 
-def test_make_model(tiny_dataset):
-    model = SHMoofModel(tiny_dataset)
-    assert tiny_dataset.max_length == model.site_count
+
+def test_resolve_padded_kmer(tiny_model):
+    twomers = ["".join(l) for l in itertools.product(BASES, repeat=2)]
+    assert tiny_model.resolve_padded_kmer("NNT") == ["".join(s) + "T" for s in twomers]
+    assert tiny_model.resolve_padded_kmer("NAT") == [s + "AT" for s in BASES]
+    assert tiny_model.resolve_padded_kmer("ATN") == ["AT" + s for s in BASES]
+
+
+def test_run_model_forward(tiny_dataset, tiny_model):
+    assert tiny_dataset.max_length == tiny_model.site_count
     encoded_parent, _, _ = tiny_dataset[0]
-    model.forward(encoded_parent)
+    tiny_model.forward(encoded_parent)
+
 
 def test_run():
-    train_dataframe = pd.DataFrame({"parent": ["ATGTA", "GTAC"], "child": ["ACGTA", "TACG"]})
-    val_dataframe = pd.DataFrame({"parent": ["ATGTA", "GTAA"], "child": ["ACGTA", "TACG"]})
+    train_dataframe = pd.DataFrame(
+        {"parent": ["ATGTA", "GTAC"], "child": ["ACGTA", "TACG"]}
+    )
+    val_dataframe = pd.DataFrame(
+        {"parent": ["ATGTA", "GTAA"], "child": ["ACGTA", "TACG"]}
+    )
     burrito = SHMoofBurrito(train_dataframe, val_dataframe, max_length=6, kmer_length=3)
     burrito.train(epochs=5)
     burrito.write_shmoof_output("_ignore")
