86013272e021335d218cc089c6d148774709edb7
Erick Matsen <ematsen@gmail.com>
2023-11-14 14:26:37 -0800

    welcome noof

---
+++ netam/noof.py
@@ -0,0 +1,132 @@
+import itertools
+
+import pandas as pd
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import DataLoader
+
+from epam.torch_common import PositionalEncoding
+
+BASES = ["A", "C", "G", "T"]
+
+
+class NoofModel(nn.Module):
+    def __init__(
+        self, dataset, embedding_dim, nhead, dim_feedforward, layer_count, dropout=0.5
+    ):
+        super(NoofModel, self).__init__()
+        self.kmer_to_index = dataset.kmer_to_index
+        self.kmer_count = len(dataset.kmer_to_index)
+        self.embedding_dim = embedding_dim
+        self.site_count = dataset.max_length
+
+        self.kmer_embedding = nn.Embedding(self.kmer_count, self.embedding_dim)
+        self.pos_encoder = PositionalEncoding(self.embedding_dim, dropout)
+
+        self.encoder_layer = TransformerEncoderLayer(
+            d_model=self.embedding_dim,
+            nhead=nhead,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+            batch_first=True,
+        )
+        self.encoder = TransformerEncoder(self.encoder_layer, layer_count)
+        self.linear = nn.Linear(self.embedding_dim, 1)
+
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, encoded_parents):
+        # encoded_parents is expected to have dimensions [batch_size, sequence_length]
+        kmer_embeddings = self.kmer_embedding(encoded_parents)
+        kmer_embeddings = self.pos_encoder(kmer_embeddings)
+
+        # Pass through the transformer encoder
+        transformer_output = self.encoder(kmer_embeddings)
+
+        # Apply the linear layer and squeeze out the last dimension
+        # After the linear layer, the dimensions will be [batch_size, sequence_length, 1]
+        # We squeeze out the last dimension to make it [batch_size, sequence_length]
+        log_rates = self.linear(transformer_output).squeeze(-1)
+
+        rates = torch.exp(log_rates)
+
+        return rates
+
+
+class NoofBurrito:
+    def __init__(
+        self,
+        train_dataset,
+        val_dataset,
+        model,
+        batch_size=32,
+        learning_rate=0.01,
+        l2_regularization_coeff=1e-6,
+    ):
+        self.train_loader = DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=True
+        )
+        self.val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        self.model = model
+        self.criterion = nn.BCELoss()
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=learning_rate,
+            weight_decay=l2_regularization_coeff,
+        )
+
+    def train(self, epochs):
+        self.model.train()
+        training_losses = []
+        validation_losses = []
+
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for encoded_parents, masks, mutation_indicators in self.train_loader:
+                loss = self._calculate_loss(encoded_parents, masks, mutation_indicators)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                running_loss += loss.item()
+
+            epoch_loss = running_loss / len(self.train_loader.dataset)
+            training_losses.append(epoch_loss)
+
+            # Validation phase
+            self.model.eval()
+            validation_loss = 0.0
+            with torch.no_grad():
+                for encoded_parents, masks, mutation_indicators in self.val_loader:
+                    loss = self._calculate_loss(
+                        encoded_parents, masks, mutation_indicators
+                    )
+                    validation_loss += loss.item()
+            validation_loss /= len(self.val_loader.dataset)
+            validation_losses.append(validation_loss)
+
+            print(
+                f"Epoch [{epoch+1}/{epochs}]\t Loss: {epoch_loss:.8f}\t Val Loss: {validation_loss:.8f}"
+            )
+
+        return pd.DataFrame(
+            {"training_losses": training_losses, "validation_losses": validation_losses}
+        )
+
+    def _calculate_loss(self, encoded_parents, masks, mutation_indicators):
+        rates = self.model(encoded_parents)
+        mutation_freq = mutation_indicators / masks.sum(dim=1, keepdim=True)
+        mut_prob = 1 - torch.exp(-rates * mutation_freq)
+        mut_prob_masked = mut_prob[masks]
+        mutation_indicator_masked = mutation_indicators[masks].float()
+        return self.criterion(mut_prob_masked, mutation_indicator_masked)
