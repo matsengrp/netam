40a79469979870ed0786e0a558ff3ae929df62d5
Erick Matsen <ematsen@gmail.com>
2024-01-25 01:47:22 -0800

    cleaning up assertion

--- netam/dnsm.py
+++ netam/dnsm.py
@@ -286,12 +286,8 @@ class DNSMBurrito(framework.Burrito):
         predictions = predictions.masked_select(mask)
         aa_subs_indicator = aa_subs_indicator.masked_select(mask)
 
+        assert torch.isfinite(predictions).all()
         predictions = clamp_probability(predictions)
-
-        if not ((predictions >= 0.0).all() and (predictions <= 1.0).all()):
-            raise ValueError(
-                f"Predictions out of range: {predictions.min()}, {predictions.max()}"
-            )
 
         return self.bce_loss(predictions, aa_subs_indicator)
 
