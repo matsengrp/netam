f2333dd0344185e046b8590c2266d78b142b4a9a
Erick Matsen <ematsen@gmail.com>
2024-06-28 04:53:58 -0700

    comment fix

--- netam/framework.py
+++ netam/framework.py
@@ -423,8 +423,7 @@ class Burrito(ABC):
 
         # copied from # https://github.com/karpathy/nanoGPT/blob/9755682b981a45507f6eb9b11eadef8cb83cebd5/model.py#L264
         param_dict = {pn: p for pn, p in self.named_parameters() if p.requires_grad}
-        # create optim groups. Any >= 2D parameters will be weight decayed, otherwise no.
-        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        # Do not apply weight decay to 1D parameters (biases and layernorm weights).
         decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
         nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
         optim_groups = [
