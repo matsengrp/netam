8c280f0c09520ae27c186423915c80d52320a64c
Erick Matsen <ematsen@gmail.com>
2024-09-27 05:36:08 -0700

    more refactorin'

--- netam/molevol.py
+++ netam/molevol.py
@@ -65,7 +65,7 @@ def build_mutation_matrices(
     probabilities for each parent codon along the sequence, this function
     constructs a sequence of 3x4 matrices. Each matrix in the sequence
     represents the mutation probabilities for each nucleotide position in a
-    parent codon. The ijkth entry of the resulting tensor corresponds to the 
+    parent codon. The ijkth entry of the resulting tensor corresponds to the
     probability of the jth nucleotide in the ith parent codon mutating to the
     kth nucleotide (in indices).
 
@@ -364,11 +364,39 @@ def neutral_aa_probs(
     return aa_probs
 
 
+def mut_probs_of_aa_probs(
+    parent_codon_idxs: Tensor,
+    aa_probs: Tensor,
+) -> Tensor:
+    """For every site, what is the probability that the amino acid will have a
+    substution or mutate to a stop under neutral evolution?
+
+    Args:
+        parent_codon_idxs (torch.Tensor): The parent codons for each sequence. Shape: (codon_count, 3)
+        aa_probs (torch.Tensor): The probability that each site will change to each amino acid. Shape: (codon_count, 20)
+    """
+    # We build a table that will allow us to look up the amino acid index
+    # from the codon indices. Argmax gets the aa index.
+    aa_idx_from_codon = CODON_AA_INDICATOR_MATRIX.argmax(dim=1).view(4, 4, 4)
+
+    # Get the amino acid index for each parent codon.
+    parent_aa_idxs = aa_idx_from_codon[
+        (
+            parent_codon_idxs[:, 0],
+            parent_codon_idxs[:, 1],
+            parent_codon_idxs[:, 2],
+        )
+    ]
+    p_staying_same = aa_probs[(torch.arange(len(parent_aa_idxs)), parent_aa_idxs)]
+
+    return 1.0 - p_staying_same
+
+
 def neutral_aa_mut_probs(
     parent_codon_idxs: Tensor,
     codon_mut_probs: Tensor,
     codon_sub_probs: Tensor,
-    ) -> Tensor:
+) -> Tensor:
     """For every site, what is the probability that the amino acid will have a
     substution or mutate to a stop under neutral evolution?
 
@@ -385,24 +413,10 @@ def neutral_aa_mut_probs(
         torch.Tensor: The probability that each site will change to some other amino acid.
                       Shape: (codon_count,)
     """
-   
+
     aa_probs = neutral_aa_probs(parent_codon_idxs, codon_mut_probs, codon_sub_probs)
-   
-    # Next we build a table that will allow us to look up the amino acid index
-    # from the codon indices. Argmax gets the aa index.
-    aa_idx_from_codon = CODON_AA_INDICATOR_MATRIX.argmax(dim=1).view(4, 4, 4)
-
-    # Get the amino acid index for each parent codon.
-    parent_aa_idxs = aa_idx_from_codon[
-        (
-            parent_codon_idxs[:, 0],
-            parent_codon_idxs[:, 1],
-            parent_codon_idxs[:, 2],
-        )
-    ]
-    p_staying_same = aa_probs[(torch.arange(len(parent_aa_idxs)), parent_aa_idxs)]
-
-    return 1.0 - p_staying_same
+    mut_probs = mut_probs_of_aa_probs(parent_codon_idxs, aa_probs)
+    return mut_probs
 
 
 def mutsel_log_pcp_probability_of(sel_matrix, parent, child, rates, sub_probs):
