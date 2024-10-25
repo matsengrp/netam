9f2dccb06f26eed3969da3a26b8aedbb3d5cdec3
Erick Matsen <ematsen@gmail.com>
2024-10-25 13:27:14 -0700

    half-temporary

--- netam/dnsm.py
+++ netam/dnsm.py
@@ -445,20 +445,20 @@ class DNSMBurrito(framework.Burrito):
 
     def find_optimal_branch_lengths(self, dataset, **optimization_kwargs):
         worker_count = min(mp.cpu_count() // 2, 10)
-        # # The following can be used when one wants a better traceback.
-        # burrito = DNSMBurrito(None, dataset, copy.deepcopy(self.model))
-        # return burrito.serial_find_optimal_branch_lengths(dataset, **optimization_kwargs)
-        our_optimize_branch_length = partial(
-            worker_optimize_branch_length,
-            self.__class__,
-        )
-        with mp.Pool(worker_count) as pool:
-            splits = dataset.split(worker_count)
-            results = pool.starmap(
-                our_optimize_branch_length,
-                [(self.model, split, optimization_kwargs) for split in splits],
-            )
-        return torch.cat(results)
+        # The following can be used when one wants a better traceback.
+        burrito = self.__class__(None, dataset, copy.deepcopy(self.model))
+        return burrito.serial_find_optimal_branch_lengths(dataset, **optimization_kwargs)
+#         our_optimize_branch_length = partial(
+#             worker_optimize_branch_length,
+#             self.__class__,
+#         )
+#         with mp.Pool(worker_count) as pool:
+#             splits = dataset.split(worker_count)
+#             results = pool.starmap(
+#                 our_optimize_branch_length,
+#                 [(self.model, split, optimization_kwargs) for split in splits],
+#             )
+#         return torch.cat(results)
 
     def to_crepe(self):
         training_hyperparameters = {
