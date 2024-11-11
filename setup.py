c7efad50988ce7e502d3a4ccc26d8a5f868c122d
Will Dumm <wdumm88@gmail.com>
2024-11-11 11:49:24 -0900

    add workflow and setuptools-scm

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="netam",
-    version="0.1.0",
+    # version="0.1.0",
     url="https://github.com/matsengrp/netam.git",
     author="Matsen Group",
     author_email="ematsen@gmail.com",
     description="Neural network models for antibody affinity maturation",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    python_requires=">=3.7",
     packages=find_packages(),
     install_requires=[
         "biopython",
