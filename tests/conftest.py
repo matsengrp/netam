37d3e4cdbc902459275fcdab4555397ae7a44749
Will Dumm <wrhdumm@gmail.com>
2024-12-23 10:38:02 -0800

    Add paired test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,20 @@ from netam import pretrained
 
 @pytest.fixture(scope="module")
 def pcp_df():
-    # TODO add some checking related to joined mode
     df = load_pcp_df(
         "data/wyatt-10x-1p5m_pcp_2023-11-30_NI.first100.csv.gz",
+    )
+    df = add_shm_model_outputs_to_pcp_df(
+        df,
+        pretrained.load("ThriftyHumV0.2-45"),
+    )
+    return df
+
+
+@pytest.fixture(scope="module")
+def pcp_df_paired():
+    df = load_pcp_df(
+        "data/wyatt-10x-1p5m_paired-merged_fs-all_pcp_2024-11-21_no-naive_sample100.csv.gz",
     )
     df = add_shm_model_outputs_to_pcp_df(
         df,
