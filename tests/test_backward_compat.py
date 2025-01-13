24fe7109ae7bd720f5a355e7b5655eb6d325494b
Will Dumm <wrhdumm@gmail.com>
2025-01-13 14:01:57 -0800

    fix old model test

--- tests/test_backward_compat.py
+++ tests/test_backward_compat.py
@@ -1,65 +1,19 @@
-import os
 
 import torch
-import pytest
 
-from netam.common import BIG, force_spawn
-from netam.framework import (
-    crepe_exists,
-    load_crepe,
-    load_pcp_df,
-)
-from netam.sequences import MAX_AA_TOKEN_IDX, translate_sequence
-from netam.models import TransformerBinarySelectionModelWiggleAct
-from netam.dasm import (
-    DASMBurrito,
-    DASMDataset,
-    zap_predictions_along_diagonal,
-)
+from netam.framework import load_crepe
+from netam.sequences import set_wt_to_nan
 
 
-
-# @pytest.fixture(scope="module")
-# def dasm_old_burrito(pcp_df):
-#     force_spawn()
-#     """Fixture that returns the DNSM Burrito object."""
-#     pcp_df["in_train"] = True
-#     pcp_df.loc[pcp_df.index[-15:], "in_train"] = False
-#     train_dataset, val_dataset = DASMDataset.train_val_datasets_of_pcp_df(pcp_df)
-
-#     model = load_crepe("old_models/dasm_13kv1jaffe+v1tang-joint")
-
-#     burrito = DASMBurrito(
-#         train_dataset,
-#         val_dataset,
-#         model,
-#         batch_size=32,
-#         learning_rate=0.001,
-#         min_learning_rate=0.0001,
-#     )
-#     burrito.joint_train(
-#         epochs=1, cycle_count=2, training_method="full", optimize_bl_first_cycle=False
-#     )
-#     return burrito
-
-def test_old_model_outputs(pcp_df):
+def test_old_model_outputs():
     example_seq = "QVQLVESGGGVVQPGRSLRLSCAASGFTFSSSGMHWVRQAPGKGLEWVAVIWYDGSNKYYADSVKGRFTISRDNSKNTVYLQMNSLRAEDTAVYYCAREGHSNYPYYYYYMDVWGKGTTVTVSS"
     dasm_crepe = load_crepe("tests/old_models/dasm_13k-v1jaffe+v1tang-joint")
     dnsm_crepe = load_crepe("tests/old_models/dnsm_13k-v1jaffe+v1tang-joint")
 
-    unmodified_df = load_pcp_df(
-        "data/wyatt-10x-1p5m_pcp_2023-11-30_NI.first100.csv.gz",
-    )
-    # evaluate on first sequence in pcp_df:
-    sequence = unmodified_df["parent"].iloc[0]
-    aa_sequence = translate_sequence(sequence)
-    print(aa_sequence)
-    print(dasm_crepe([example_seq]))
-    assert False
+    dasm_vals = torch.nan_to_num(set_wt_to_nan(torch.load("tests/old_models/dasm_output", weights_only=True), example_seq), 0.0)
+    dnsm_vals = torch.load("tests/old_models/dnsm_output", weights_only=True)
 
-
-    # aa_parents_idxs = pcp_df["aa_parents_idxs"].iloc[0]
-
-    # Test applying models to raw sequences (keep in mind that pcp_df may have
-    # tokens added, such as separator tokens, to sequences), and to sequences that have been
-    # processed by the Dataset initializer.
+    dasm_result = torch.nan_to_num(dasm_crepe([example_seq])[0], 0.0)
+    dnsm_result = dnsm_crepe([example_seq])[0]
+    assert torch.allclose(dasm_result, dasm_vals)
+    assert torch.allclose(dnsm_result, dnsm_vals)
