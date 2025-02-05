5078cf9fa9710fc6fa85f2ec7fd8c9bd7120fc5e
Will Dumm <wrhdumm@gmail.com>
2025-02-05 09:12:24 -0800

    add branch length and prediction output comparison test (#109)
    Also fixes a bug in the `dataset.to` method.

--- tests/test_backward_compat.py
+++ tests/test_backward_compat.py
@@ -1,7 +1,58 @@
 import torch
+import pandas as pd
+import pytest
+from netam.dasm import zap_predictions_along_diagonal, DASMBurrito, DASMDataset
+from netam.common import force_spawn
+from tqdm import tqdm
 
 from netam.framework import load_crepe
 from netam.sequences import set_wt_to_nan
+
+
+@pytest.fixture(scope="module")
+def fixed_dasm_val_burrito(pcp_df):
+    force_spawn()
+    """Fixture that returns the DNSM Burrito object."""
+    pcp_df["in_train"] = True
+    pcp_df.loc[pcp_df.index[-15:], "in_train"] = False
+    dasm_crepe = load_crepe("tests/old_models/dasm_13k-v1jaffe+v1tang-joint")
+    model = dasm_crepe.model
+    train_dataset, val_dataset = DASMDataset.train_val_datasets_of_pcp_df(
+        pcp_df, model.known_token_count
+    )
+
+    burrito = DASMBurrito(
+        train_dataset,
+        val_dataset,
+        model,
+        batch_size=32,
+        learning_rate=0.001,
+        min_learning_rate=0.0001,
+    )
+    burrito.standardize_and_optimize_branch_lengths()
+    return burrito
+
+
+def test_predictions_of_batch(fixed_dasm_val_burrito):
+    # These outputs were produced by the comparison code in this test, but
+    # written to the files referenced here. The code state was netam 3c632fa.
+    # (however, this test did not exist in the codebase at that time)
+    branch_lengths = torch.tensor(
+        pd.read_csv("tests/old_models/val_branch_lengths.csv")["branch_length"]
+    ).double()
+    these_branch_lengths = fixed_dasm_val_burrito.val_dataset.branch_lengths.double()
+    assert torch.allclose(branch_lengths, these_branch_lengths)
+    fixed_dasm_val_burrito.model.eval()
+    val_loader = fixed_dasm_val_burrito.build_val_loader()
+    predictions_list = []
+    for batch in tqdm(val_loader, desc="Calculating model predictions"):
+        predictions = zap_predictions_along_diagonal(
+            fixed_dasm_val_burrito.predictions_of_batch(batch), batch["aa_parents_idxs"]
+        )
+        predictions_list.append(predictions.detach().cpu())
+    these_predictions = torch.cat(predictions_list, axis=0).double()
+    predictions = torch.load("tests/old_models/val_predictions.pt").double()
+    assert torch.allclose(predictions, these_predictions)
 
 
 # The outputs used for this test are produced by running
