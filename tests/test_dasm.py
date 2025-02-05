7a7e58437536b698e20342b6a3210900fd484bbd
Will Dumm <wrhdumm@gmail.com>
2025-02-05 14:19:24 -0800

    rearrange tests to prep for dropping ddsm

--- tests/test_dasm.py
+++ tests/test_dasm.py
@@ -1,19 +1,32 @@
+import os
+
 import torch
 import pytest
 
-from netam.common import force_spawn
-from netam.sequences import MAX_KNOWN_TOKEN_COUNT
+from netam.framework import (
+    crepe_exists,
+    load_crepe,
+)
+from netam.common import BIG, force_spawn
 from netam.models import TransformerBinarySelectionModelWiggleAct
 from netam.dasm import (
     DASMBurrito,
     DASMDataset,
 )
+from netam.dxsm import zap_predictions_along_diagonal
+from netam.sequences import (
+    MAX_KNOWN_TOKEN_COUNT,
+    AA_AMBIG_IDX,
+    TOKEN_STR_SORTED,
+    token_mask_of_aa_idxs,
+    CODON_AA_INDICATOR_MATRIX,
+)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", params=["pcp_df", "pcp_df_paired"])
 def dasm_burrito(pcp_df):
     force_spawn()
-    """Fixture that returns the DNSM Burrito object."""
+    """Fixture that returns the DASM Burrito object."""
     pcp_df["in_train"] = True
     pcp_df.loc[pcp_df.index[-15:], "in_train"] = False
     train_dataset, val_dataset = DASMDataset.train_val_datasets_of_pcp_df(
@@ -47,3 +60,73 @@ def test_parallel_branch_length_optimization(dasm_burrito):
     parallel_branch_lengths = dasm_burrito.find_optimal_branch_lengths(dataset)
     branch_lengths = dasm_burrito.serial_find_optimal_branch_lengths(dataset)
     assert torch.allclose(branch_lengths, parallel_branch_lengths)
+
+
+def test_split_recombine(dasm_burrito):
+    # This is a silly test, but it helped me catch a bug resulting from
+    # re-computing mask from nt strings with tokens stripped out in the split
+    # method, so I'm leaving it in.
+    dataset = dasm_burrito.val_dataset
+    splits = dataset.split(2)
+    parallel_tokens = torch.concat(
+        [token_mask_of_aa_idxs(dset.aa_parents_idxss) for dset in splits]
+    )
+    tokens = token_mask_of_aa_idxs(dataset.aa_parents_idxss)
+    assert torch.allclose(tokens, parallel_tokens)
+
+
+def test_crepe_roundtrip(dasm_burrito):
+    os.makedirs("_ignore", exist_ok=True)
+    crepe_path = "_ignore/ddsm"
+    dasm_burrito.save_crepe(crepe_path)
+    assert crepe_exists(crepe_path)
+    crepe = load_crepe(crepe_path)
+    model = crepe.model
+    assert isinstance(model, TransformerBinarySelectionModelWiggleAct)
+    assert dasm_burrito.model.hyperparameters == model.hyperparameters
+    model.to(dasm_burrito.device)
+    for t1, t2 in zip(
+        dasm_burrito.model.state_dict().values(), model.state_dict().values()
+    ):
+        assert torch.equal(t1, t2)
+
+
+def test_zap_diagonal(dasm_burrito):
+    batch = dasm_burrito.val_dataset[0:2]
+    codon_predictions = dasm_burrito.predictions_of_batch(batch)
+    predictions = torch.log((torch.exp(codon_predictions) @ CODON_AA_INDICATOR_MATRIX))
+    aa_parents_idxs = batch["aa_parents_idxs"].to(dasm_burrito.device)
+    # These sites are set to NaN, so we need to make them zero for comparison
+    invalid_mask = aa_parents_idxs >= AA_AMBIG_IDX  # Shape: [B, L]
+    predictions[invalid_mask] = 0.0
+    zeroed_predictions = predictions.clone()
+    zeroed_predictions = zap_predictions_along_diagonal(
+        zeroed_predictions, aa_parents_idxs
+    )
+    print(predictions.shape, aa_parents_idxs.shape)
+    print(predictions)
+    print(aa_parents_idxs)
+    L = predictions.shape[1]
+    for batch_idx in range(2):
+        for i in range(L):
+            for j in range(20):
+                if j == aa_parents_idxs[batch_idx, i]:
+                    assert zeroed_predictions[batch_idx, i, j] == -BIG
+                else:
+                    assert (
+                        zeroed_predictions[batch_idx, i, j]
+                        == predictions[batch_idx, i, j]
+                    )
+
+
+def test_selection_factors_of_aa_str(dasm_burrito):
+    parent_aa_idxs = dasm_burrito.val_dataset.aa_parents_idxss[0]
+    aa_parent = "".join(TOKEN_STR_SORTED[i] for i in parent_aa_idxs)
+    # This won't work if we start testing with ambiguous sequences
+    aa_parent = aa_parent.replace("X", "")
+    aa_parent_pair = tuple(aa_parent.split("^"))
+    res = dasm_burrito.model.selection_factors_of_aa_str(aa_parent_pair)
+    assert len(res[0]) == len(aa_parent_pair[0])
+    assert len(res[1]) == len(aa_parent_pair[1])
+    assert res[0].shape[1] == 20
+    assert res[1].shape[1] == 20
--- tests/test_ddsm.py
+++ tests/test_ddsm.py
@@ -1,23 +1,15 @@
-import os
-
 import torch
 import pytest
 
-from netam.common import BIG, force_spawn
-from netam.framework import (
-    crepe_exists,
-    load_crepe,
-)
+from netam.common import force_spawn
 from netam.models import TransformerBinarySelectionModelWiggleAct
 from netam.ddsm import (
     DDSMBurrito,
     DDSMDataset,
-    zap_predictions_along_diagonal,
 )
 from netam.sequences import (
     MAX_KNOWN_TOKEN_COUNT,
     TOKEN_STR_SORTED,
-    token_mask_of_aa_idxs,
 )
 
 torch.set_printoptions(precision=10)
@@ -53,79 +45,6 @@ def ddsm_burrito(pcp_df):
         epochs=1, cycle_count=2, training_method="full", optimize_bl_first_cycle=False
     )
     return burrito
-
-
-def test_parallel_branch_length_optimization(ddsm_burrito):
-    dataset = ddsm_burrito.val_dataset
-    parallel_branch_lengths = ddsm_burrito.find_optimal_branch_lengths(dataset)
-    branch_lengths = ddsm_burrito.serial_find_optimal_branch_lengths(dataset)
-    assert torch.allclose(branch_lengths, parallel_branch_lengths)
-
-
-def test_split_recombine(ddsm_burrito):
-    # This is a silly test, but it helped me catch a bug resulting from
-    # re-computing mask from nt strings with tokens stripped out in the split
-    # method, so I'm leaving it in.
-    dataset = ddsm_burrito.val_dataset
-    splits = dataset.split(2)
-    parallel_tokens = torch.concat(
-        [token_mask_of_aa_idxs(dset.aa_parents_idxss) for dset in splits]
-    )
-    tokens = token_mask_of_aa_idxs(dataset.aa_parents_idxss)
-    assert torch.allclose(tokens, parallel_tokens)
-
-
-def test_crepe_roundtrip(ddsm_burrito):
-    os.makedirs("_ignore", exist_ok=True)
-    crepe_path = "_ignore/ddsm"
-    ddsm_burrito.save_crepe(crepe_path)
-    assert crepe_exists(crepe_path)
-    crepe = load_crepe(crepe_path)
-    model = crepe.model
-    assert isinstance(model, TransformerBinarySelectionModelWiggleAct)
-    assert ddsm_burrito.model.hyperparameters == model.hyperparameters
-    model.to(ddsm_burrito.device)
-    for t1, t2 in zip(
-        ddsm_burrito.model.state_dict().values(), model.state_dict().values()
-    ):
-        assert torch.equal(t1, t2)
-
-
-def test_zap_diagonal(ddsm_burrito):
-    batch = ddsm_burrito.val_dataset[0:2]
-    predictions = ddsm_burrito.predictions_of_batch(batch)
-    predictions = torch.cat(
-        [predictions, torch.zeros_like(predictions[:, :, :1])], dim=-1
-    )
-    aa_parents_idxs = batch["aa_parents_idxs"].to(ddsm_burrito.device)
-    zeroed_predictions = predictions.clone()
-    zeroed_predictions = zap_predictions_along_diagonal(
-        zeroed_predictions, aa_parents_idxs
-    )
-    L = predictions.shape[1]
-    for batch_idx in range(2):
-        for i in range(L):
-            for j in range(20):
-                if j == aa_parents_idxs[batch_idx, i]:
-                    assert zeroed_predictions[batch_idx, i, j] == -BIG
-                else:
-                    assert (
-                        zeroed_predictions[batch_idx, i, j]
-                        == predictions[batch_idx, i, j]
-                    )
-
-
-def test_selection_factors_of_aa_str(ddsm_burrito):
-    parent_aa_idxs = ddsm_burrito.val_dataset.aa_parents_idxss[0]
-    aa_parent = "".join(TOKEN_STR_SORTED[i] for i in parent_aa_idxs)
-    # This won't work if we start testing with ambiguous sequences
-    aa_parent = aa_parent.replace("X", "")
-    aa_parent_pair = tuple(aa_parent.split("^"))
-    res = ddsm_burrito.model.selection_factors_of_aa_str(aa_parent_pair)
-    assert len(res[0]) == len(aa_parent_pair[0])
-    assert len(res[1]) == len(aa_parent_pair[1])
-    assert res[0].shape[1] == 20
-    assert res[1].shape[1] == 20
 
 
 def test_build_selection_matrix_from_parent(ddsm_burrito):
