better tests and fix sim masking

netam/framework.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import pickle
 import copy
 import os
 from time import time
@@ -22,6 +23,7 @@ from netam.common import (
     tensor_to_np_if_needed,
     BIG,
     parallelize_function,
+    clamp_probability,
 )
 from netam.sequences import (
     codon_mask_tensor_of,
@@ -36,8 +38,11 @@ from netam.sequences import (
     nt_mask_tensor_of,
     encode_sequences,
     translate_sequences,
+    translate_sequences_mask_codons,
     CODONS,
     aa_idx_tensor_of_str,
+    aa_idx_tensor_of_str_ambig,
+    nt_idx_tensor_of_str,
 )
 from netam import models
 import netam.molevol as molevol
@@ -1205,6 +1210,11 @@ def worker_optimize_branch_length(burrito_class, model, dataset, optimization_kw
     return burrito.serial_find_optimal_branch_lengths(dataset, **optimization_kwargs)
 
 
+def _nan_masked_sites(in_tensor, site_mask):
+    in_tensor[~site_mask] = float("nan")
+    return in_tensor
+
+
 def codon_probs_of_parent_seq(
     selection_crepe, nt_sequence, branch_length, neutral_crepe=None, multihit_model=None
 ):
@@ -1226,7 +1236,10 @@ def codon_probs_of_parent_seq(
             "nt_sequence must be a pair of strings, with the first element being the heavy chain sequence and the second element being the light chain sequence."
         )
 
-    aa_seqs = tuple(translate_sequences(nt_sequence))
+    # We need to translate codons containing N's as ambiguities, even if they
+    # can be translated to a single amino acid. Otherwise, predictions will not
+    # match the burrito predictions
+    aa_seqs = tuple(translate_sequences_mask_codons(nt_sequence))
     # We must mask any codons containing N's because we need neutral probs to
     # do simulation:
     mask = tuple(codon_mask_tensor_of(chain_nt_seq) for chain_nt_seq in nt_sequence)
@@ -1270,17 +1283,87 @@ def codon_probs_of_parent_seq(
     )
 
     return tuple(
-        molevol.zero_stop_codon_probs(
+        _nan_masked_sites(molevol.zero_stop_codon_probs(
             molevol.adjust_codon_probs_by_aa_selection_factors(
                 chain_parent_codon_idxs,
                 chain_log_codon_probs,
                 chain_log_aa_selection_factors,
             ).exp()
-        )
-        for chain_parent_codon_idxs, chain_log_codon_probs, chain_log_aa_selection_factors in zip(
-            parent_codon_idxs, log_codon_probs, log_selection_factors
+        ), chain_mask)
+        for chain_parent_codon_idxs, chain_log_codon_probs, chain_log_aa_selection_factors, chain_mask in zip(
+            parent_codon_idxs, log_codon_probs, log_selection_factors, mask
         )
     )
+
+
+def codon_probs_of_parent_seq_new(
+    selection_crepe, nt_sequence, branch_length, neutral_crepe=None, multihit_model=None
+):
+    """Calculate the predicted model probabilities of each codon at each site.
+
+    Args:
+        nt_sequence: A tuple of two strings, the heavy and light chain nucleotide
+            sequences.
+        branch_length: The branch length of the tree.
+    Returns:
+        a tuple of tensors of shape (L, 64) representing the predicted probabilities of each
+        codon at each site.
+    """
+    if neutral_crepe is None:
+        raise NotImplementedError("neutral_crepe is required.")
+
+    if isinstance(nt_sequence, str) or len(nt_sequence) != 2:
+        raise ValueError(
+            "nt_sequence must be a pair of strings, with the first element being the heavy chain sequence and the second element being the light chain sequence."
+        )
+
+    aa_seqs = tuple(translate_sequences_mask_codons(nt_sequence))
+    # We must mask any codons containing N's because we need neutral probs to
+    # do simulation:
+    mask = tuple(codon_mask_tensor_of(chain_nt_seq) for chain_nt_seq in nt_sequence)
+    # TODO This does not apply light chain correction factor!!
+    rates, csps = trimmed_shm_model_outputs_of_crepe(neutral_crepe, nt_sequence)
+
+    selection_factors = selection_crepe([aa_seqs])[0]
+
+    if selection_crepe.model.hyperparameters["output_dim"] == 1:
+        # Need to upgrade single selection factor to 20 selection factors, all
+        # equal except for the one for the parent sequence, which should be
+        # 1 (0 in log space).
+        new_selection_factors = []
+        for aa_seq, old_selection_factors in zip(aa_seqs, selection_factors):
+            if len(aa_seq) == 0:
+                new_selection_factors.append(torch.empty(0, 20, dtype=old_selection_factors.dtype))
+            else:
+                parent_indices = aa_idx_tensor_of_str_ambig(aa_seq)
+                new_selection_factors.append(
+                    molevol.lift_to_per_aa_selection_factors(old_selection_factors, parent_indices)
+                )
+        selection_factors = tuple(new_selection_factors)
+
+    parent_nt_idxs = tuple(
+        nt_idx_tensor_of_str(nt_chain_seq.replace("N", "A")) for nt_chain_seq in nt_sequence
+    )
+    codon_probs = []
+    for parent_idxs, nt_csps, nt_rates, sel_matrix in zip(parent_nt_idxs, csps, rates, selection_factors):
+        if len(parent_idxs) > 0:
+            nt_mut_probs = 1.0 - torch.exp(-branch_length * nt_rates)
+            with open("there_inputs.p", "wb") as f:
+                pickle.dump((parent_idxs, nt_mut_probs, nt_csps, sel_matrix), f)
+            codon_mutsel, _ = molevol.build_codon_mutsel(
+                parent_idxs.reshape(-1, 3),
+                nt_mut_probs.reshape(-1, 3),
+                nt_csps.reshape(-1, 3, 4),
+                sel_matrix,
+                multihit_model=multihit_model,
+            )
+            with open("there_outputs.p", "wb") as f:
+                pickle.dump(codon_mutsel, f)
+            codon_probs.append(molevol.flatten_codons(clamp_probability(codon_mutsel)))
+        else:
+            codon_probs.append(torch.empty(0, 64, dtype=torch.float32))
+
+    return tuple(codon_probs)
 
 
 def sample_sequence_from_codon_probs(codon_probs):

tests/test_simulation.py
@@ -11,6 +11,7 @@ from netam.framework import (
     codon_probs_of_parent_seq,
     trimmed_shm_model_outputs_of_crepe,
     sample_sequence_from_codon_probs,
+    load_crepe,
 )
 from netam import pretrained
 from netam.common import force_spawn, clamp_probability, clamp_log_probability
@@ -22,18 +23,22 @@ from netam.dnsm import (
     DNSMBurrito,
     DNSMDataset,
 )
+import netam.molevol as molevol
 from netam.molevol import (
     neutral_codon_probs_of_seq,
     zero_stop_codon_probs,
     set_parent_codon_prob,
 )
+from netam.models import DEFAULT_MULTIHIT_MODEL
 
 from netam.models import TransformerBinarySelectionModelWiggleAct
+import netam.sequences as sequences
 from netam.sequences import (
     MAX_KNOWN_TOKEN_COUNT,
     aa_mask_tensor_of,
     nt_idx_tensor_of_str,
     translate_sequence,
+    translate_sequence_mask_codons,
     translate_sequences,
     iter_codons,
     hamming_distance,
@@ -563,13 +568,13 @@ def test_sequence_sample_dnsm(pcp_df, dnsm_burrito):
         sample_sequence_from_codon_probs(heavy_codon_probs)
 
 
-def introduce_ns(sequence):
+def introduce_ns(sequence, p=0.1):
     """Introduce N's into the sequence."""
     # Convert the sequence to a list of characters
     seq_list = list(sequence)
     # Randomly select positions to introduce N's
     for i in range(len(seq_list)):
-        if torch.rand(1).item() < 0.1:  # 10% chance to introduce an N
+        if torch.rand(1).item() < p:  # 10% chance to introduce an N
             seq_list[i] = "N"
     # Convert back to a string
     return "".join(seq_list)
@@ -603,3 +608,179 @@ def test_ambig_sample_dnsm(pcp_df, dnsm_burrito):
                     assert c == "NNN", f"Codon {p} should be NNN, but got {c}"
                 else:
                     assert "N" not in c, f"Codon {c} should not contain N, but got {p}"
+
+
+
+
+# TODO these will need to be upgraded to dynamically created burritos to keep
+# the test up to date. However, for speed I'll just read from disk:
+def dasm_burrito(pcp_df):
+    crepe = load_crepe("/home/wdumm/dnsm-netam-proj-runner1/dnsm-experiments-1/tests/models/dasm_13k-v1tangCC-joint")
+    return DASMBurrito(
+        None,
+        DASMDataset.of_pcp_df(
+            pcp_df,
+            crepe.model.known_token_count,
+            multihit_model=None,
+        ),
+        model=crepe.model,
+    )
+
+
+def dnsm_burrito(pcp_df):
+    crepe = load_crepe("/home/wdumm/dnsm-netam-proj-runner1/dnsm-experiments-1/tests/models/dnsm_77k-v1jaffe50k-joint")
+    return DNSMBurrito(
+        None,
+        DNSMDataset.of_pcp_df(
+            pcp_df,
+            crepe.model.known_token_count,
+            multihit_model=None,
+        ),
+        model=crepe.model,
+    )
+
+
+# TODO add dasm_burrito back
+@pytest.mark.parametrize("burrito_func", [dnsm_burrito])
+def test_selection_factors(pcp_df, burrito_func):
+    burrito = burrito_func(pcp_df)
+    # There are two ways of computing codon probabilities. Let's make sure
+    # they're the same:
+    pcp_df = pcp_df.copy()
+    pcp_df["parent_h"] = [introduce_ns(seq) for seq in pcp_df["parent_h"]]
+    neutral_crepe = pretrained.load("ThriftyHumV0.2-59")
+    pcp_df = add_shm_model_outputs_to_pcp_df(
+        pcp_df.copy(),
+        neutral_crepe,
+    )
+    multihit_model = pretrained.load_multihit(DEFAULT_MULTIHIT_MODEL)
+    for multihit_model in [None, pretrained.load_multihit(DEFAULT_MULTIHIT_MODEL)]:
+        branch_length = 0.8
+        for seq in pcp_df["parent_h"]:
+            parent_idxs = sequences.nt_idx_tensor_of_str(seq.replace("N", "A"))
+            aa_seq_len = len(seq) // 3
+            aa_parent = translate_sequence_mask_codons(seq)
+            aa_parent_idxs = sequences.aa_idx_tensor_of_str_ambig(aa_parent)
+            codon_parent_idxs = sequences.codon_idx_tensor_of_str_ambig(seq)
+            hit_classes = parent_specific_hit_classes(
+                parent_idxs.reshape(-1, 3),
+            )
+            flat_hit_classes = molevol.flatten_codons(hit_classes)
+
+            _token_nt_parent, _ = sequences.prepare_heavy_light_pair(
+                seq,
+                "",
+                burrito.model.known_token_count,
+            )
+            _token_aa_parent = translate_sequence(_token_nt_parent)
+            _token_aa_parent_idxs = sequences.aa_idx_tensor_of_str_ambig(_token_aa_parent)
+            _token_aa_mask = sequences.codon_mask_tensor_of(_token_nt_parent)
+            print("from burrito")
+            sel_matrix = burrito.build_selection_matrix_from_parent_aa(_token_aa_parent_idxs, _token_aa_mask)[:len(aa_parent)]
+
+            print("from crepe")
+            from_crepe = burrito.to_crepe()([(aa_parent, "")])[0][0]
+            print(from_crepe)
+            from_crepe = molevol.lift_to_per_aa_selection_factors(from_crepe, sequences.aa_idx_tensor_of_str_ambig(aa_parent))
+            if not torch.allclose(from_crepe, sel_matrix):
+                print(from_crepe)
+                print(sel_matrix)
+                assert False
+
+
+
+
+# TODO add dasm_burrito back
+@pytest.mark.parametrize("burrito_func", [dasm_burrito, dnsm_burrito])
+def test_build_codon_mutsel(pcp_df, burrito_func):
+    burrito = burrito_func(pcp_df)
+    # There are two ways of computing codon probabilities. Let's make sure
+    # they're the same:
+    neutral_crepe = pretrained.load("ThriftyHumV0.2-59")
+    pcp_df = pcp_df.copy()
+    pcp_df["parent_h"] = [introduce_ns(seq) for seq in pcp_df["parent_h"]]
+    pcp_df = add_shm_model_outputs_to_pcp_df(
+        pcp_df.copy(),
+        neutral_crepe,
+    )
+    multihit_model = pretrained.load_multihit(DEFAULT_MULTIHIT_MODEL)
+    for multihit_model in [None, pretrained.load_multihit(DEFAULT_MULTIHIT_MODEL)]:
+        branch_length = 0.06
+        for seq, nt_rates, nt_csps in zip(pcp_df["parent_h"], pcp_df["nt_rates_h"], pcp_df["nt_csps_h"]):
+            parent_idxs = sequences.nt_idx_tensor_of_str(seq.replace("N", "A"))
+            aa_seq_len = len(seq) // 3
+            aa_parent = translate_sequence(seq)
+            aa_parent_idxs = sequences.aa_idx_tensor_of_str_ambig(aa_parent)
+            codon_parent_idxs = sequences.codon_idx_tensor_of_str_ambig(seq)
+            hit_classes = parent_specific_hit_classes(
+                parent_idxs.reshape(-1, 3),
+            )
+            flat_hit_classes = molevol.flatten_codons(hit_classes)
+
+            _token_nt_parent, _ = sequences.prepare_heavy_light_pair(
+                seq,
+                "",
+                burrito.model.known_token_count,
+            )
+            _token_aa_parent = translate_sequence(_token_nt_parent)
+            _token_aa_parent_idxs = sequences.aa_idx_tensor_of_str_ambig(_token_aa_parent)
+            _token_aa_mask = sequences.codon_mask_tensor_of(_token_nt_parent)
+            aa_mask = _token_aa_mask[:len(aa_parent)]
+            # sel_matrix = torch.ones((aa_seq_len, 20))
+            # This is in linear space.
+            sel_matrix = burrito.build_selection_matrix_from_parent_aa(_token_aa_parent_idxs, _token_aa_mask)[:len(aa_parent)]
+            # neutral_sel_matrix[torch.arange(aa_seq_len), aa_parent_idxs]
+
+            # First way:
+            nt_mut_probs = 1.0 - torch.exp(-branch_length * nt_rates)
+            codon_mutsel, _ = molevol.build_codon_mutsel(
+                parent_idxs.reshape(-1, 3),
+                nt_mut_probs.reshape(-1, 3),
+                nt_csps.reshape(-1, 3, 4),
+                sel_matrix,
+                multihit_model=multihit_model,
+            )
+            log_codon_mutsel = clamp_probability(codon_mutsel).log()
+            flat_log_codon_mutsel = molevol.flatten_codons(log_codon_mutsel)
+            flat_log_codon_mutsel[~aa_mask] = float("nan")
+
+            # Second way:
+            neutral_codon_probs = molevol.neutral_codon_probs_of_seq(
+                seq,
+                aa_mask,
+                nt_rates,
+                nt_csps,
+                branch_length,
+                multihit_model=multihit_model,
+            )
+            adjusted_codon_probs = molevol.adjust_codon_probs_by_aa_selection_factors(
+                codon_parent_idxs.unsqueeze(0),
+                neutral_codon_probs.unsqueeze(0).log(),
+                sel_matrix.unsqueeze(0).log()
+            ).squeeze(0)
+
+            # Now let's compare to the simulation probs:
+            sim_probs = clamp_probability(codon_probs_of_parent_seq(
+                burrito.to_crepe(),
+                (seq, ""),
+                branch_length,
+                neutral_crepe=neutral_crepe,
+                multihit_model=multihit_model,
+            )[0]).log()
+
+            if not torch.allclose(adjusted_codon_probs, sim_probs, equal_nan=True):
+                diff_mask = ~torch.isclose(adjusted_codon_probs, sim_probs, equal_nan=True)
+                print(flat_hit_classes[diff_mask])
+                print((adjusted_codon_probs - sim_probs)[diff_mask])
+                print(adjusted_codon_probs[diff_mask])
+                print(sim_probs[diff_mask])
+                assert False
+
+            # Compare mutsel path with adjust by selection factors path:
+            if not torch.allclose(adjusted_codon_probs, flat_log_codon_mutsel, equal_nan=True):
+                diff_mask = ~torch.isclose(adjusted_codon_probs, flat_log_codon_mutsel, equal_nan=True)
+                print(flat_hit_classes[diff_mask])
+                print((adjusted_codon_probs - flat_log_codon_mutsel)[diff_mask])
+                print(adjusted_codon_probs[diff_mask])
+                print(flat_log_codon_mutsel[diff_mask])
+                assert False